d44204002e67113f9c5eb72ea717c1276e1d6cd8
saloni776 <saloni.aryal@westcliff.edu>
2025-03-15 22:19:58 +0545

    Add files via upload

---
+++ evenoddchecker.js
@@ -0,0 +1,10 @@
+function checkEvenOrOdd(number) {
+    if (number % 2 === 0) {
+        return `${number} is an even number.`;
+    } else {
+        return `${number} is an odd number.`;
+    }
+}
+
+console.log(checkEvenOrOdd(7)); 
+console.log(checkEvenOrOdd(8)); 
